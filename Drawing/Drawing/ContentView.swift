a785aa1b24fbd7a5dcee48d28217812ae7186058
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-16 19:55:00 +0530

    Adding Arrow and animation for arrow from the challenge

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,13 +7,55 @@
 
 import SwiftUI
 
+struct Arrow: InsettableShape{
+    var insetAmount = 0.0
+    
+    var animatableData: Double {
+            get { insetAmount }
+            set { insetAmount = newValue}
+        }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.height - insetAmount))
+        path.addLine(to: CGPoint(x: rect.midX, y: insetAmount))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.height * 0.33))
+        path.move(to: CGPoint(x: rect.midX, y: insetAmount))
+        path.addLine(to: CGPoint(x: rect.width - insetAmount, y: rect.height * 0.33))
+        path.closeSubpath()
+        
+        return path
+        
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arrow = self
+        arrow.insetAmount += amount
+        return arrow
+    }
+}
+
 struct ContentView: View {
+    @State private var lineWidth = 1.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Spacer()
+            VStack{
+                Arrow()
+                    .stroke(.blue, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                    .animation(.easeInOut(duration: 2), value: lineWidth)
+                    .padding()
+            }
+            .frame(width: 300, height: 300)
+            .border(.gray)
+            
+            Spacer()
+            
+            Text("Line width: \(lineWidth, specifier: "%.00f")")
+            Slider(value: $lineWidth, in: 1...20)
+                .padding()
         }
         .padding()
     }
