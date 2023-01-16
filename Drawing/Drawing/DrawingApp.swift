c41f7f2385778f1dfd65338278d4a1646c800029
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-16 15:36:59 +0530

    Adding Drawing project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Faizaan Khan on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
