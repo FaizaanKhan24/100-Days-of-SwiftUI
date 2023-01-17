8c3db8a1a65f8ff7939bfb9efd2bdc56fd55ce62
Faizaan Khan <faizaankhan24@gmail.com>
2023-01-17 12:50:34 +0530

    Adding initial file for Habit tracker

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Faizaan Khan on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
