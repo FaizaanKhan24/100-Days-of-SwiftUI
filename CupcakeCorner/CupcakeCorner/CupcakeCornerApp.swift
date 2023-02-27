55b05aba66bee022820867304152f24c1ee406a5
Faizaan Khan <faizaankhan24@gmail.com>
2023-02-27 09:09:19 -0500

    Project 10 Setup and progress
    Progress of Day 49 and Day 50

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Faizaan Khan on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
