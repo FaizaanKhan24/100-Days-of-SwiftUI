f1cfd0cacf3425057a954d1ff87b3fb50a44d936
Faizaan Khan <faizaankhan24@gmail.com>
2023-03-01 08:35:05 -0500

    Project 10 Challenge 1

--- CupcakeCorner/CupcakeCorner/Order.swift
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -34,7 +34,7 @@ class Order : ObservableObject, Codable {
     @Published var zip = ""
     
     var hasValidAddress: Bool {
-        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+        if name.isEmpty || streetAddress.trimmingCharacters(in: .whitespaces).isEmpty || city.isEmpty || zip.isEmpty {
             return false
         }
 
