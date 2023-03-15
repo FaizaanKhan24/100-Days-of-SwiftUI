2dde99d4b2fc3b94fcdb0919b702644feb5c9fcb
Faizaan Khan <faizaankhan24@gmail.com>
2023-03-15 09:34:03 -0400

    Progress: Day 70

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Faizaan Khan on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
