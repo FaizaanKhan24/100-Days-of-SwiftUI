7caccba7befacc3297afd3e75deda743a6b724d9
Faizaan Khan <faizaankhan24@gmail.com>
2023-03-20 08:36:13 -0400

    Accessibility app setup

---
+++ AccessibilityProject/AccessibilityProject/AccessibilityProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityProjectApp.swift
+//  AccessibilityProject
+//
+//  Created by Faizaan Khan on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
